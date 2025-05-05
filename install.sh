cf6444eb02a0dacb79b7eb35d726b705b8fd4619
Nam Nguyen <me@namnd.com>
2025-05-05 15:43:44 +1000

    fix: symlink install script

--- install.sh
+++ install.sh
@@ -4,12 +4,6 @@
 # Bootstrapping NixOS
 #################################################
 
-nix-shell -p git
-
-git clone --recurse-submodules https://github.com/namnd/nixos
-
-cd nixos || return
-
 sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
 sudo ln -s "$PWD"/configuration.nix /etc/nixos/configuration.nix
 
@@ -19,7 +13,8 @@ sudo nixos-rebuild switch
 # Install home-manager
 #################################################
 
-ln -sfn ./home-manager ./config/home-manager
+mkdir -p "$HOME"/.config
+ln -sfn "$PWD"/home-manager "$HOME"/.config/home-manager
 
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz home-manager
 nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
@@ -33,13 +28,16 @@ home-manager switch
 #################################################
 # Fonts
 #################################################
-ln -sfn ./fonts ./local/share/fonts
+
+mkdir -p "$HOME"/.local/share
+ln -sfn "$PWD"/fonts "$HOME"/.local/share/fonts
+sudo fc-cache
 
 
 #################################################
 # Others
 #################################################
 
-ln -sfn ./nixpkgs ./config/nixpkgs
-ln -sfn ./nvim/ ./config/nvim
-ln -sfn ./ghostty ./config/ghostty
+ln -sfn "$PWD"/nixpkgs "$HOME"/.config/nixpkgs
+ln -sfn "$PWD"/nvim "$HOME"/.config/nvim
+ln -sfn "$PWD"/ghostty "$HOME"/.config/ghostty
