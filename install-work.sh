c24182ade056f716f4a23e03c65dc1feb0bd91af
Nam Nguyen <me@namnd.com>
2025-05-13 16:59:57 +1000

    fix install script order

--- install-work.sh
+++ install-work.sh
@@ -10,7 +10,11 @@ sh <(curl -L https://nixos.org/nix/install) --daemon
 # Install home-manager
 #################################################
 
+mv "$HOME"/.bashrc "$HOME"/.bashrc.bak
+mv "$HOME"/.profile "$HOME"/.profile.bak
+
 mkdir -p "$HOME"/.config
+ln -sfn "$PWD"/nixpkgs "$HOME"/.config/nixpkgs
 ln -sfn "$PWD"/home-manager-work "$HOME"/.config/home-manager
 
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz home-manager
@@ -30,7 +34,8 @@ sudo fc-cache
 # Others
 #################################################
 
-ln -sfn "$PWD"/nixpkgs "$HOME"/.config/nixpkgs
 ln -sfn "$PWD"/nvim "$HOME"/.config/nvim
+
+rm -rf "$HOME"/.config/ghostty
 ln -sfn "$PWD"/ghostty "$HOME"/.config/ghostty
 ln -sfn "$PWD"/xsessionrc "$HOME"/.xsessionrc
